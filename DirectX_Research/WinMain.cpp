912c48848fffd244933eff07fe70b98e26c737b7
justindd1994 <justindd1994@gmail.com>
2020-01-07 02:22:07 +0200

    -Initial push.

---
+++ DirectX_Research/WinMain.cpp
@@ -0,0 +1,59 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+		case WM_CLOSE:
+			PostQuitMessage(0);
+			break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	const auto pClassName = "Direct_X_Research";
+
+	//register windows class
+	WNDCLASSEX wc = {0};
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+
+	//Create window instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		"Direct X Research",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr);
+
+	//Show the window
+	ShowWindow(hWnd, SW_SHOW);
+
+	//Message Pump
+	MSG msg;
+	BOOL gResult;
+
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+		return -1;
+
+	return msg.wParam;
+}
