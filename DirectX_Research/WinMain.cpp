f563f3df5c4390420a00d9127cd8d09de54ed798
Justindd1994 <justindd1994@gmail.com>
2020-01-16 02:53:52 +0200

    -Added mouse events to the window, it can now detect mouse scroll, movements and clicks.

--- DirectX_Research/WinMain.cpp
+++ DirectX_Research/WinMain.cpp
@@ -1,4 +1,5 @@
 #include "Window.h"
+#include <sstream>
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -12,6 +13,25 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
+
+			while (!wnd.mouse.IsEmpty())
+			{
+				const auto e = wnd.mouse.Read();
+
+				switch (e.GetType())
+				{
+				case Mouse::Event::Type::Leave:
+					wnd.SetTitle("Gone!");
+					break;
+				case Mouse::Event::Type::Move:
+					{
+						std::ostringstream oss;
+						oss << "Mouse Position: (" << e.GetPosX() << "," << e.GetPosY() << ")";
+						wnd.SetTitle(oss.str());
+					}
+				break;
+				}
+			}
 
 			if (wnd.kbd.KeyIsPressed(VK_MENU))
 			{
