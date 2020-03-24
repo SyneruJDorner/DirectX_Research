95d0a554e42ff469d480075d676c3e7aeb9b2fd1
Justindd1994 <justindd1994@gmail.com>
2020-03-24 23:51:04 +0200

    Added Constant Buffers.

--- DirectX_Research/VertexShader.hlsl
+++ DirectX_Research/VertexShader.hlsl
@@ -4,10 +4,15 @@ struct VSOut
 	float4 pos : SV_POSITION;
 };
 
+cbuffer CBuf
+{
+	row_major matrix transform;
+};
+
 VSOut main(float2 pos : POSITION, float3 color : COLOR)
 {
 	VSOut vso;
-	vso.pos = float4(pos.x, pos.y, 0.0f, 1.0f);
+	vso.pos = mul(float4(pos.x, pos.y, 0.0f, 1.0f), transform);
 	vso.color = color;
 	return vso;
 }
